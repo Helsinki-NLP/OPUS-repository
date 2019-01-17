f5c631767f9dbc0a8f7fe0df5878f03f6135e1f6
Jörg Tiedemann <jorg.tiedemann@helsinki.fi>
2019-01-17 12:09:38 +0000

    unicode ...

--- perllib/LetsMT/lib/LetsMT/Import/tar.pm
+++ perllib/LetsMT/lib/LetsMT/Import/tar.pm
@@ -199,16 +199,19 @@ sub initialize_import{
 	## --> use utf8::decode instead to avoid breaking
 	## --> find some more principles way to handle unicode ...
 	# $response = decode( 'utf8', $response );
-	my $DecodeSuccess = utf8::decode($response);
+	# my $DecodeSuccess = utf8::decode($response);
 
-	my $XmlParser = new XML::LibXML;
-	my $dom       = $XmlParser->parse_string($response);
-	my @nodes     = $dom->findnodes('//list/entry/imported_from');
-	foreach my $n (@nodes){
-	    my ( $tarfile, $file ) = split( /\:/, $n->to_literal() );
-	    ## TODO: try again to decode
-	    utf8::decode($file) unless ($DecodeSuccess);
-	    push( @{$self->{success}}, $file );
+	## NEW: do this only if decoding succeeds
+	if (utf8::decode($response)){
+	    my $XmlParser = new XML::LibXML;
+	    my $dom       = $XmlParser->parse_string($response);
+	    my @nodes     = $dom->findnodes('//list/entry/imported_from');
+	    foreach my $n (@nodes){
+		my ( $tarfile, $file ) = split( /\:/, $n->to_literal() );
+		## TODO: try again to decode?
+		# utf8::decode($file) unless ($DecodeSuccess);
+		push( @{$self->{success}}, $file );
+	    }
 	}
     }
 
